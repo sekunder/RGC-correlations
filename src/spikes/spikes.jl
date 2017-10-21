b319d4c9e9da24067cf2a9f7e14a89c0acdcc762
Alex Kunin <abk170@psu.edu>
2017-10-20 23:13:48 -0400

    boilerplate stuff for Spikes module

---
+++ src/spikes/spikes.jl
@@ -0,0 +1,38 @@
+"""
+Spikes
+
+Includes types and methods for handling spike trains.
+"""
+module Spikes
+#TODO take some time to write down what I actually need from this module. For
+#instance, raster/histogram are the most apparent, but it might be nice to have
+#some other tools on hand.
+
+#TODO don't get carried away though.
+
+#TODO implement the things that need implementing.
+
+import Base.show
+
+################################################################################
+### Abstract type just in case
+################################################################################
+
+"""
+    AbstractSpikeTrains
+
+Abstract type for spike trains
+"""
+abstract AbstractSpikeTrains
+
+################################################################################
+#### Miscellaneous functions and constants
+################################################################################
+
+################################################################################
+#### Include appropriate files
+################################################################################
+
+export AbstractSpikeTrains
+
+end
