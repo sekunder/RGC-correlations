553cfd715b5356579bae9de6199595029f0ca6b9
Alex Kunin <abk170@psu.edu>
2017-10-23 12:00:53 -0400

    implemented CRCNS_Stimulus type

--- src/stimulus/CRCNS_Stimulus.jl
+++ src/stimulus/CRCNS_Stimulus.jl
@@ -1,31 +1,90 @@
+
+using MAT
+
 """
-    CRCNS_Stimulus(filename, rec_idx; kwargs...)
+    CRCNS_Stimulus(file_name, rec_idx; kwargs...)
 
 A wrapper class for manipulating/computing with the stimulus used in the CRCNS
 experiments.
 """
 type CRCNS_Stimulus <: AbstractStimulus
-    Bits::BitMatrix
-    # TODO more fields go here
+    file_loc::String
+    file_name::String
+    rec_idx::Int # internally-used index for accessing array/dictionary elements
+    rec_no::Int  # The actual recording number per the experimenter's labeling.
+    bits::BitMatrix
+    N::Tuple{Int,Int}
+    px::Tuple{Int,Int}
+    d::Tuple{Int,Int}
+    mm_per_px::Float64
+    frame_length_s::Float64
+    onset::Float64
+    metadata::Dict{Any,Any}
 
+    function CRCNS_Stimulus(mat_file::String, recording_index::Int; verbose=false, kwargs...)
+        fname = basename(mat_file)
+        floc = dirname(abspath(mat_file))
+        if verbose
+            println("Reading CRCNS stimulus data from file: $(joinpath(floc, fname))")
+        end
+        vars = matread(joinpath(floc, fname))
 
-    function CRCNS_Stimulus(filename::String, recording_index::Int; kwargs...)
-        # TODO write this function, and decide what fields this type needs
+        px = (Int(vars["stimulus"]["param"][recording_index]["x"]), Int(vars["stimulus"]["param"][recording_index]["y"]))
+        d = (Int(vars["stimulus"]["param"][recording_index]["dx"]), Int(vars["stimulus"]["param"][recording_index]["dy"]))
+        N = px ./ d
+        mm_per_px = vars["stimulus"]["pixelsize"][recording_index] / 1000.0
+        frame_length_s = vars["stimulus"]["frame"][recording_index]
+        onset = vars["stimulus"]["onset"][recording_index]
+
+        if verbose
+            println("Loading binary data from $(joinpath(floc,"..","ran1.bin"))")
+        end
+        bin_file = joinpath(floc,"..","ran1.bin")
+
+        # now load the frames. remember that read! will throw an error if the
+        # number of bits to read is not 0 mod 64 and the bits beyond that are
+        # nonzero. It's a bug with Julia but it's not too bad to work around.
+        N_frames = Int(vars["stimulus"]["Nframes"][recording_index])
+        temp_N_frames = N_frames
+        while mod(prod(N) * temp_N_frames, 64) != 0
+            temp_n_frames += 1
+        end
+        stimulus = falses(prod(N), temp_N_frames)
+        read!(bin_file, stimulus)
+        stimulus = stimulus[:,1:N_frames]
+
+        new(floc, fname, recording_index, vars["datainfo"]["RecNo"][recording_index],
+            stimulus, N, px, d, mm_per_px, frame_length_s, onset, Dict(kwargs))
     end
 end
 
+frame_size(S::CRCNS_Stimulus) = S.px
+frame_time(S::CRCNS_Stimulus) = S.frame_length_s
+
+matrix_form(S::CRCNS_Stimulus) = (-1.0) .^ (!S.bits)
+
 function show(io::IO, S::CRCNS_Stimulus)
-    # TODO display useful information.
+    println(io, "CRCNS binary whitenoise stimulus")
+    println(io, "Used for file $(S.file_name), recording $(S.rec_no)")
+    println(io, "Frame size (w,h): $(frame_size(S)) pixels, $(S.mm_per_px .* frame_size(S)) mm")
+    println(io, "Framerate: $(framerate(S)) Hz ($(frametime(S)) s/frame)")
 end
 
-function frame_matrix(S::CRCNS_Stimulus, I)
-    # TODO return a 3d array (3rd dim might be 1, or omitted?)
-end
+"""
+    frame_image(S::CRCNS_Stimulus, idx::Int)
+    frame_image(S::CRCNS_Stimulus, t::Float64, relative_time=false)
 
-function frame_vector(S::CRCNS_Stimulus, I)
-    # TODO return a 2d array (might be a vector)
-end
+Returns a matrix of size `reverse(S.px)` which is the actual image displayed on
+screen. `idx` is the index of the frame, while `t` is the time of display. If
+`relative_time` is `true`, time `t = 0.0` corresponds to stimulus onset.
 
-function frame_image(S::CRCNS_Stimulus, I)
-    # TODO return a 3d array (3rd dim might be 1?)
+"""
+# TODO all of these will need checking to make sure I am consistent in reshaping stuff.
+function frame_image(S::CRCNS_Stimulus, idx::Int)
+    # first get the column from bits
+    # frame_vec = S.bits[:,idx]
+    frame = (-1.0) .^ (!reshape_frame_vec(S, S.bits[:,idx]))
+    return kron(frame, ones(S.d))
 end
+frame_image(S::CRCNS_Stimulus, t::Float64, relative_time::Bool=false) = frame_image(S, ceil(Int, (relative_time ? t - S.onset : t)/frame_time(S)))
+reshape_frame_vec(S::CRCNS_Stimulus, fv) = reshape(fv, reverse(S.N))
--- src/stimulus/stimulus.jl
+++ src/stimulus/stimulus.jl
@@ -2,7 +2,8 @@
 Stimulus
 
 A wrapper class and API for handling stimuli without needing to worry about the
-specific implementation for that experiment.
+specific implementation for that experiment. Basically, I want to be able to
+call `compute_STRFs(raster, stimulus)` regardless of the data source.
 """
 module Stimulus
 
@@ -22,7 +23,17 @@ abstract AbstractStimulus
 #### Miscellaneous functions and constants
 ################################################################################
 
-function compute_STRFs(spike_raster::Union{Matrix{Bool}, BitMatrix}, stim::AbstractStimulus; kwargs...)
+default_CRCNS_dir = ispath("/Users/sekunder/Documents/MATLAB/crcns_ret-1") ? "/Users/sekunder/Documents/MATLAB/crcns_ret-1" : "/data1/homes/abk170/crcns_ret-1"
+
+"""
+    framerate(S::AbstractStimulus)
+
+Convenience method returns `1/frametime(S)`. Each concrete subtype of
+AbstractStimulus should implement `frametime`.
+"""
+frame_rate(S::AbstractStimulus) = 1.0 / frametime(S)
+
+function _compute_STRFs(spike_histogram::Matrix{Float64}, stim::AbstractStimulus; kwargs...)
     # TODO implement this. The idea is that frame_vector will return a matrix
     # where each column is one frame of the stimulus, so this function should
     # generically work on any stimulus with frame_vector implemented.
@@ -33,6 +44,7 @@ end
 ################################################################################
 include("CRCNS_Stimulus.jl")
 
-export frame_matrix, frame_vector, frame_image,
+export show, frame_size, frame_time, frame_rate, matrix_form, frame_image,
        CRCNS_Stimulus
+
 end
