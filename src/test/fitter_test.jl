3193a554d796b17d395b5a56007acf1b203c1eed
Alex Kunin <abk170@psu.edu>
2017-10-30 18:55:43 -0400

    testing manual gradient descent

--- src/test/fitter_test.jl
+++ src/test/fitter_test.jl
@@ -35,7 +35,45 @@ toc()
 println(P_1)
 
 println("Second order model (i.e. Ising model)")
-tic()
-P_2 = second_order_model(X)
-toc()
-println(P_2)
+println("Don't forget, I commented out a line about vector storage.")
+# tic()
+# P_2 = second_order_model(X, verbose=true, force_MPF=true)
+# toc()
+# println(P_2)
+F_X(J,g) = loglikelihood(X, J, g; mu_X =X * X' / N_samples)
+N_neurons, N_samples = size(X)
+
+begin
+    n_attempts=1000
+    print_attempts=10
+    lr = 4.0; whoopscount=0;
+    println("Gonna just do gradient descent for $n_attempts steps with a learning rate")
+    Jseed = rand(N_neurons,N_neurons); Jseed = (Jseed + Jseed') / (2 * N_neurons)
+    J_0 = Jseed[:]
+    g = zeros(N_neurons^2)
+
+    F_vals = zeros(0)
+    J_vals=Vector{Vector{Float64}}(0)
+    g_vals = Vector{Vector{Float64}}(0)
+
+    push!(J_vals, J_0)
+    push!(F_vals, F_X(J_0, g))
+    push!(g_vals, g)
+    max_F = F_vals[1]; max_F_idx = 1;
+    attempt = 2
+    println("Step\tF_X(J,θ)\tdF\t|gradient|\tlr")
+    while attempt <= n_attempts
+        push!(J_vals, J_vals[attempt-1] + lr * g_vals[attempt-1])
+        push!(F_vals, F_X(J_vals[attempt], g))
+        push!(g_vals, g)
+        if mod(attempt, print_attempts) == 0
+            println("$attempt\t$(F_vals[attempt])\t$(F_vals[attempt]-F_vals[attempt-1])\t$(sqrt(dot(g,g)))\t$lr")
+        end
+        if F_vals[attempt] < F_vals[attempt]
+            whoopscount += 1
+            # lr = 2.0 - exp(-whoopscount)
+            lr /= 2.0
+        end
+        attempt += 1
+    end
+end
