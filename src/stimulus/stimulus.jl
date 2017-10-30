b0680806358d197bd926ecb426121a44fc5e0c78
Alex Kunin <abk170@psu.edu>
2017-10-30 15:43:03 -0400

    renamed show_metadata.jl
    in case I want to add functions later, like add_metadata!() or things
    like that.

--- src/stimulus/stimulus.jl
+++ src/stimulus/stimulus.jl
@@ -22,7 +22,7 @@ abstract AbstractStimulus
 ################################################################################
 #### Miscellaneous functions and constants
 ################################################################################
-include("../util/show_metadata.jl")
+include("../util/metadata.jl")
 
 default_CRCNS_dir = ispath("/Users/sekunder/Documents/MATLAB/crcns_ret-1") ? "/Users/sekunder/Documents/MATLAB/crcns_ret-1" : "/data1/homes/abk170/crcns_ret-1"
 
