fc74aaf3ebc7d5c2bae7e9e8951b9915f216f50a
Alex Kunin <abk170@psu.edu>
2017-11-11 13:59:27 -0500

    Revert "Revert "random_subset, TODO list""
    This reverts commit 548dc736d6d4892d556cbd4614f3d97162efa059.

--- src/util/misc.jl
+++ src/util/misc.jl
@@ -11,3 +11,45 @@ decimal_round(x::Float64, k::Integer) = x > 0 ? ceil(x * 10.0^k) / 10.0^k : floo
 Removes the trailing ".ext" from `fname`
 """
 remove_extension(fname) = join(split(fname,'.')[1:end-1],'.')
+
+"""
+    random_subset(A,k)
+
+Returns a random subset of k elements from collection A. Uses Reservoir
+Sampling, see https://en.wikipedia.org/wiki/Reservoir_sampling#Algorithm_R
+
+"""
+function random_subset(A,k::Integer)
+    n = length(A)
+    # assert(n >= k, "Reservoir Sample error: To few items to sample from")
+    R = Array(typeof(A[1]), k)
+    R[1:k] = A[1:k]
+    for i = (k+1):n
+        j = rand(1:i)
+        if j <= k
+            R[j] = A[i]
+        end
+    end
+return R
+end
+
+"""
+    random_subset!(A,R)
+
+Returns a random subset of length(R) elements from collection A, modifying R in
+place. See `random_subset` for more details.
+
+"""
+function random_subset!(A, R::Vector)
+    n = length(A)
+    k = length(R)
+    # assert(n >= k, "Reservoir Sample error: Reservoir too large")
+    R[1:k] = A[1:k]
+    for i = (k+1):n
+        j = rand(1:i)
+        if j <= k
+            R[j] = A[i]
+        end
+    end
+    return R
+end
