6d582eb1cf0f7a37236c493bd5ab033dcaeb882b
Alex Kunin <abk170@psu.edu>
2017-11-20 19:12:11 -0500

    verbosity update: help_string

--- src/scripts/CRCNS_generate_STRFs.jl
+++ src/scripts/CRCNS_generate_STRFs.jl
@@ -1,4 +1,22 @@
+help_string = """
+$(@__FILE__)
 
+Computes STRFs for CRCNS data, then generates simulated data by presenting
+stimulus to those STRFs.
+
+Usage:
+  [julia] $(@__FILE__) --defaults | --dir data_dir | file1 file2 ... [options]
+  Processes files specified by first argument/flags:
+    --defaults : read all .mat files in CRCNS_Data_dir, also sets output directories
+    --dir data_dir : real all .mat files in dir
+    file1 file2 ... : read files in list from CRCNS_Data_dir
+  Options:
+    --verbose 0|1|2 : controls verbosity of output
+    --output_dir_real path, --output_dir_sim path : where to write .jld files
+    --skip file1 file2 ... : which files to skip (including overriding defaults)
+    --poisson_time t, --poisson_loops n, --poisson_spikes m : Stop poisson process generation if it lasts longer than t seconds, n loops, or m spikes
+    --help : display this message and exit
+"""
 # CRCNS_script_version=v"0.3"
 # plan: for each mat file, for each recording index:
 # 1. load the stimulus, call CRCNS_output_STRFs to save them to file
@@ -14,27 +32,21 @@ include("../util/nonlinearities.jl")
 # include("../util/constants.jl")
 # using Spikes, Stimulus, Probability
 
-cline_args = process_args(ARGS; parse_flags=["verbose", "poisson_time","poisson_loops","poisson_spikes"], bool_flags=["defaults"])
+cline_args = process_args(ARGS; parse_flags=["verbose", "poisson_time","poisson_loops","poisson_spikes"], bool_flags=["help","defaults"])
 verbose = get(cline_args,"verbose",[0])[1]
 poisson_time = Float64(get(cline_args,"poisson_time",[0.0])[1])
 poisson_loops = get(cline_args,"poisson_loops",[0])[1]
 poisson_spikes = get(cline_args,"poisson_spikes",[0])[1]
 
+if cline_args["help"]
+    println(help_string)
+    println("VERSION: $CRCNS_script_version")
+    exit(0)
+end
+
 println("-" ^ 80)
 println("CRCNS_generate_STRFs $CRCNS_script_version")
-println("Usage:")
-println("  $(@__FILE__) --defaults | --dir data_dir | file1 file2 ... [options]")
-println("\tProcesses files specified by first argument/flags:")
-println("\t\t--defaults : read all .mat files in CRCNS_Data_dir, also sets output directories")
-println("\t\t--dir data_dir : real all .mat files in dir")
-println("\t\tfile1 file2 ... : read files in list from CRCNS_Data_dir")
-println("\tOptions:")
-println("\t\t--verbose 0|1|2 : controls verbosity of output")
-println("\t\t--output_dir_real path, --output_dir_sim path : where to write .jld files")
-println("\t\t--skip file1 file2 ... : which files to skip (including overriding defaults)")
-println("\t\t--poisson_time t, --poisson_loops n, --poisson_spikes m : Stop poisson process generation if it lasts longer than t seconds, n loops, or m spikes")
-println()
-println("Computing STRFs for CRCNS data, then generating simulated data by presenting stimulus to those STRFs.")
+# println(help_string)
 
 data_dir = get(cline_args,"dir",[CRCNS_data_dir])[1]
 output_dir_real = get(cline_args, "output_dir_real", [joinpath(CRCNS_STRF_dir, "real")])[1]
