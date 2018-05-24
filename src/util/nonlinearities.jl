65ef84cafb778f19e1676181288da94bd8d64848
Alex Kunin <abk170@psu.edu>
2018-05-24 19:02:24 -0400

    missed a spot

--- src/util/nonlinearities.jl
+++ src/util/nonlinearities.jl
@@ -8,4 +8,4 @@ Parameterized sigmoid.
 f(x; θ) = θ_1 / (1 + exp(-θ_2 * (x - θ_3)))
 ```
 """
-@everywhere sigmoid(x, theta::Vector{Float64}=[1.0,1.0,0.0]) = theta[1] ./ (1 + exp.(-theta[2] .* (x .- theta[3])))
+sigmoid(x, theta::Vector{Float64}=[1.0,1.0,0.0]) = theta[1] ./ (1 + exp.(-theta[2] .* (x .- theta[3])))
