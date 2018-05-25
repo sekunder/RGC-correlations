1158b58fe608ea16eedcd2aacd0c869258bb8c87
Alex Kunin <abk170@psu.edu>
2018-05-25 16:48:31 -0400

    added automatic updating of packages

--- src/util/init.jl
+++ src/util/init.jl
@@ -19,6 +19,7 @@ for (pkg_name, pkg_url) in unregistered_packages
     if !(pkg_name in keys(Pkg.installed()))
         Pkg.clone(pkg_url)
     end
+    Pkg.update(pkg_name)
 end
 @everywhere using BinaryVectorProbability, Spikes, GrayScaleStimuli
 
